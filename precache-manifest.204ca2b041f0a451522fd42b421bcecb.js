a9c10aba3514fc07326f07587c47b0b72fcd0df6
yousef <yousef.sami19@gmail.com>
2019-07-25 20:45:59 +0430

    Updates

---
+++ precache-manifest.204ca2b041f0a451522fd42b421bcecb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7c181880af5682b304eacc6e6c9b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866d59c2bf16eb755283",
+    "url": "/static/css/main.7d285901.chunk.css"
+  },
+  {
+    "revision": "1af600d1f43ff87cf9f5",
+    "url": "/static/js/2.fd9778cc.chunk.js"
+  },
+  {
+    "revision": "866d59c2bf16eb755283",
+    "url": "/static/js/main.95a35d8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "29b77efa8dcbd41bea04aeef19e2b092",
+    "url": "/static/media/icomoon.29b77efa.eot"
+  },
+  {
+    "revision": "8e3ff1eb119b04ca0ac44329d386566f",
+    "url": "/static/media/icomoon.8e3ff1eb.woff"
+  },
+  {
+    "revision": "d08e6baff0490a5629f799fa471f1672",
+    "url": "/static/media/icomoon.d08e6baf.ttf"
+  },
+  {
+    "revision": "d8a374ed61c12836afb6e062245e6499",
+    "url": "/static/media/icomoon.d8a374ed.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
