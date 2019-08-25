3b9d63d1b137a83864835a7f457100ed3c5984ab
Ali <connexion.founder@outlook.com>
2019-08-25 17:07:47 +0200

    Add sitemap

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
+const { sitemap_en } = require('./sitemap');
 
 app.prepare().then(() => {
   const server = express();
@@ -14,6 +15,25 @@ app.prepare().then(() => {
 
   server.get('/book/:id', (req, res) => {
     return app.render(req, res, '/index', { ...query(req) });
+  });
+
+  server.get('/documentation/sitemap.xml', (req, res) => {
+    res.set('Content-Type', 'text/xml');
+
+    const buildUrl = url => {
+      return `<url>
+          <loc>https://pixelplux.com/documentation/${url.path}</loc>
+          <lastmod>2019-09-10</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.8</priority>
+       </url>`;
+    };
+
+    return res.send(`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       ${sitemap_en.map(url => buildUrl(url)).join('')}
+    </urlset> 
+    `);
   });
 
   server.get('/documentation/:id', (req, res) => {
---
+++ sitemap.js
@@ -0,0 +1,45 @@
+const sitemap_en = [
+  {
+    label: 'How to use open-source vs enterprise',
+    path: 'how-to-use-booking-and-reservation'
+  },
+  {
+    label: 'Install for ReactJs',
+    path: 'getting-started-for-reactjs'
+  },
+  {
+    label: 'ReactBooking Properties (props)',
+    path: 'react-booking-properties'
+  },
+  {
+    label: 'Custom contact information',
+    path: 'custom-contact-information'
+  },
+
+  {
+    label: 'Payment methods',
+    path: 'payment-methods'
+  },
+  {
+    label: 'Create personal booking calendar',
+    path: 'create-personal-booking-app'
+  },
+
+  {
+    label: 'Privacy Policy',
+    path: 'privacy-policy'
+  },
+  {
+    label: 'Terms and conditions',
+    path: 'terms-and-conditions'
+  },
+  {
+    label: 'Drop-in booking widget',
+    path: 'embed'
+  },
+  { path: 'on-submit-props' },
+  { path: 'payment-methods' },
+  { path: '' }
+];
+
+module.exports.sitemap_en = sitemap_en;
